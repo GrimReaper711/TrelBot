0fd1d48090524b522da6686b97611494bfccb424
Andrew Dunbar <thegrimreaper711@gmail.com>
2018-07-15 03:24:56 +1000

    Config file required for server.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'name' : 'TrelBot',
+    'port' : 6969,
+    'discordEndpoint' : 'https://discordapp.com/api/webhooks/467274025861644288/fAIlF8_xoXigkulfcvt9vVgMT2pC9L4P_AzTj_bEtTzqQIdn_aIlFKGOrXRaXHtpExDz',
+};
